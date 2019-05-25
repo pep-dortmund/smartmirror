fd1bf5b5c19d95abfaf2975fac19c49b11b579b6
Jonah Nitschke <lejonah@web.de>
2019-05-26 00:55:52 +0200

    Show Weather Info

--- smartmirror/static/site.js
+++ smartmirror/static/site.js
@@ -7,6 +7,8 @@ var app = new Vue({
   data: {
     time: null,
     departures: [],
+    cities: ['Dortmund', 'Essen', 'Bochum'],
+    weather: {},
   },
   methods: {
     updateTime: function() {
@@ -15,20 +17,40 @@ var app = new Vue({
     getDepartures() {
       axios.get('/vrr')
         .then((response) => {
-          data = response.data;
+          let data = response.data;
           data['departures'].forEach((dep, i, array) => {
             array[i]['timestamp'] = moment(dep['timestamp']);
           })
           this.departures = data['departures'];
         })
     },
+    getWeather(){
+      this.cities.forEach((city) => {
+        console.log(city);
+        axios.get('/weather/'+city)
+          .then((response) => {
+            let data = response.data['weather'];
+            this.weather[city] = {};
+            this.weather[city]['temperature'] = data['main']['temp'];
+            this.weather[city]['icon'] = '/static/images/' + data['weather'][0]['icon'] + '_inv_bw.png';
+          })
+      });
+    }
   },
   created: function() {
     this.updateTime();
     this.getDepartures();
+    this.getWeather();
   },
+  watch: {
+    cities: function (val) {
+      this.weather = {};
+      this.getWeather();
+    },
+  }
 })
 
 
 window.setInterval(app.updateTime, 1000);
 window.setInterval(app.getDepartures, 60000);
+window.setInterval(app.getWeather, 60000);
